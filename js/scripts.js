6631dee590be384888fcf72ebc10f9eac1025aaf
Austin Cummings <skwerl_e@yahoo.com>
2017-08-18 15:37:48 -0700

    reveal pictures according to scores, report scores and minor html/css/js edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,16 @@
 var firstTime = true;
-
 var pingPonged = function(inputFromUser) {
   var input1 = parseInt(inputFromUser);
-  console.log(input1);
   if (firstTime) {
     score = input1;
     firstTime = false;
-    console.log("first");
     feedback=score;
   } else {
-  score = score+input1;
-  console.log(score);
+    score = score+input1;
   }
-
-//including both variations!!!
-    if (score % 3 === 0 || score % 5 === 0) {
-        if (score % 15 === 0) {
-          feedback ="ping-pong";
-        } else if (score % 3 === 0) {
-          feedback = "ping";
-        } else if (score % 5 === 0) {
-          feedback = "pong";
-        } else {
-          feedback = score;
-        }
-
-
-
-}
-console.log("input1 " +input1);
-console.log("score "+score);
+    }
 return feedback;
 }
-
-
-
-
 
 
 
@@ -59,11 +34,14 @@ $(document).ready(function() {
   $("form#form-whole").submit(function(event) {
     event.preventDefault();
     var inputFromUser = $("#message").val();
-    var outPut= pingPonged(inputFromUser);
-    // var output = cleanSentence(inputFromUser);
-$("#result").prepend("<P>" +outPut+ "</p>");
-
-
-
+    var output= pingPonged(inputFromUser);
+    $(".pictures img").addClass("hidden");
+    $("#result").prepend("<P>" +output+ "</p>");
+    console.log(output);
+    if (output === 'ping' ||output === 'pong'||output === 'ping-pong'){
+    $("#"+output).removeClass("hidden");
+    var pictoshow=$("#"+output).removeClass("hidden");
+    console.log(pictoshow)
+  }
   });
 });
